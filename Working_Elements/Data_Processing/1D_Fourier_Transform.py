71580f9a08fd7b0bf10f85247c16fbbdf16b8ef9
Ash <ireddy.ashish.ts@yandex.ru>
2023-02-27 16:15:16 +0300

    working on 1D_Fourier_Transform

---
+++ Working_Elements/Data_Processing/1D_Fourier_Transform.py
@@ -0,0 +1,47 @@
+def logger(INPUT):
+    
+    value = []
+    
+    CC = INPUT.copy()
+    
+    for i in range(len(INPUT)):
+        value = []
+        for j in range(len(INPUT[i])):
+            if INPUT[i][j] == 0:
+                value.append(0)
+            else:
+                value.append((cmath.log10(INPUT[i][j])).real)
+    
+        CC[i] = value
+    
+    return CC
+
+
+
+# Direct FFT
+
+# This function calculates the Fourier Transform of the Data & returns they individual Columns & Rows. The Input is the source data in the form of a pandas table
+
+def mag_maker(Input):
+    
+    # columns     
+    cols = []
+    for i in range(len(Input)):
+        cols.append(rfft(Input[i]).real)
+    
+    # Rows
+    rows = []
+    for j in range(len(Input)):
+        rows.append(rfft(Input.iloc[j]).real)
+    
+    logged_cols = pd.DataFrame(logger(cols)).transpose()
+    cols_l = []
+    for i in range(len(logged_cols)):
+        cols_l.append(mean(logged_cols.iloc[i]))
+        
+    logged_rows = pd.DataFrame(logger(rows))
+    rows_l = []
+    for i in range(len(logged_rows.axes[1])):
+        rows_l.append(mean(logged_rows[i]))
+        
+    return cols_l, rows_l
