95afaf2628bc220e55984e3e0bcce30e8798f553
Ash <ireddy.ashish.ts@yandex.ru>
2023-03-16 19:40:24 +0300

    Added new clustering methods

---
+++ Clustering_Methods/Cluster_DBSCAN.py
@@ -0,0 +1,10 @@
+# https://www.kdnuggets.com/2022/08/implementing-dbscan-python.html 
+# https://towardsdatascience.com/machine-learning-clustering-dbscan-determine-the-optimal-value-for-epsilon-eps-python-example-3100091cfbc
+# https://stats.stackexchange.com/questions/88872/a-routine-to-choose-eps-and-minpts-for-dbscan 
+
+# Determining optimal EPS value: https://iopscience.iop.org/article/10.1088/1755-1315/31/1/012012/pdf
+
+# Distance can be dependent on Elements or chemicals used for the polymer
+
+# Minimal domain knowledge
+
